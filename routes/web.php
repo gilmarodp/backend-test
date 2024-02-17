b054cdb8ec42542271f149697853ad82231f7a79
Gilmar Oliveira <gilmar.odp@gmail.com>
2024-02-16 21:35:10 -0300

    ✨ feat: adicao de rotas do crud de redirects

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RedirectController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('r/{redirect}', [RedirectController::class, 'redirect'])->name('r.redirect');
+
+Route::resource('redirects', RedirectController::class)->except('show');
